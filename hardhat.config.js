56879df0f4793aaec381bdc8ec0bf759f1da80c0
Cryptopragmatic <tristan.alberio@gmail.com>
2022-09-14 13:40:54 +0800

    adding svelte

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,23 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@developerdao/hardhat-fe");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   fe: {
     version: "0.19.1-alpha",
+  },
+
+  networks: {
+    goerli: {
+      // This value will be replaced on runtime
+      url: process.env.STAGING_QUICKNODE_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    mainnet: {
+      chainId: 5,
+      url: process.env.PROD_QUICKNODE_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
   },
 };
